6661889a0a6e6f842f354cbf9b7d9ce2bef3b6ad
Loonan Chauvette <loonan.chauvette@gmail.com>
2026-05-09 17:39:05 -0400

    Fix TypeScript error: use module.exports for Vercel serverless function
    Change from 'export default' to 'module.exports' to be compatible with CommonJS module format and verbatimModuleSyntax TypeScript setting.

--- api/citation.ts
+++ api/citation.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+module.exports = async function handler(req: VercelRequest, res: VercelResponse) {
   // Only allow GET requests
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
